5ab6cd4107861a88dc869e404f18554bc44f880a
bimus-github <bimus2022@gmail.com>
2024-02-17 14:22:05 +0500

    learning

---
+++ learing/createModel.js
@@ -0,0 +1,12 @@
+function createModel() {
+  // Create a sequential model
+  const model = tf.sequential();
+
+  // Add a single input layer
+  model.add(tf.layers.dense({ inputShape: [1], units: 1, useBias: true }));
+
+  // Add an output layer
+  model.add(tf.layers.dense({ units: 1, useBias: true }));
+
+  return model;
+}
